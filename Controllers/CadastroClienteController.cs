95362963d0f04aaac42fc6bb067f85fcee74d267
RoberthKennedy <robertharaujo81@gmail.com>
2026-05-19 13:55:57 -0300

    Clientes Cadastrar

--- Controllers/CadastroClienteController.cs
+++ Controllers/CadastroClienteController.cs
@@ -7,9 +7,21 @@ namespace sistemabiblioteca.Controllers;
 
 public class CadastroClienteController : Controller
 {
+    private static List<Cliente> clientes = new List<Cliente>();
+
     public IActionResult Index()
     {
         return View();
     }
+
+    public IActionResult Cadastrar(Cliente novoCliente)
+    {
+        novocliente.Id = clientes.Count + 1;
+        clientes.Add(novocliente);
+
+        return RedirectToAction("Index");
+    }
+
+
 }
 

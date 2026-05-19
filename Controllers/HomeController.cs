39822d49eafa88a40011fb3e79e51a1cf2e2dbf4
RoberthKennedy <robertharaujo81@gmail.com>
2026-05-19 11:43:40 -0300

    Atualização

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using biblioteca.Models;
 using Microsoft.AspNetCore.Mvc;
 using sistemabiblioteca.Models;
 
@@ -8,6 +9,12 @@ public class HomeController : Controller
 {
     public IActionResult Index()
     {
+        var meuslivros = new List<Livro>();
+        {
+            new Livro { Id = 1, Titulo "Heroi das mil faces", Autor = "Joseph Campebell" }
+        new Livro { Id = 2, Titulo "O codigo da vinci", Autor = "Dan Brown" }
+        }
+        ;
         return View();
     }
 
---
+++ Models/Livros.cs
@@ -0,0 +1,8 @@
+namespace biblioteca.Models;
+
+public class Livro
+{
+  public int Id { get; set; }
+  public string Titulo { get; set; }
+  public string Autor { get; set; }
+}

4bda0192800865457644d7e75ddff9d3cc56a308
RoberthKennedy <robertharaujo81@gmail.com>
2026-05-26 10:31:52 -0300

    Atualizações

--- Models/Livros.cs
+++ Models/Livros.cs
@@ -1,8 +1,19 @@
+using System.ComponentModel.DataAnnotations;
 namespace biblioteca.Models;
 
 public class Livro
 {
+  [Key]
   public int Id { get; set; }
+  [Required]
+  [StringLength(100)]
+  [Display(Name = "Autor")]
+  [ErrorMessage = "O nome do autor deve conter entre 1 e 100 caracteres."]
+
   public string Titulo { get; set; }
+  [Required]
+  [StringLength(100)]
+  [Display(Name = "Autor")]
+  [ErrorMessage = "O nome do autor deve conter entre 1 e 100 caracteres."]
   public string Autor { get; set; }
 }
